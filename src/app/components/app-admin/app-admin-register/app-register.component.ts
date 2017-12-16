fee7dcf68ad4883a34189818eed8271ba0eb7a93
Volodymyr <wladimir.sidorow@gmail.com>
2017-12-16 16:31:38 +0200

    +

--- src/app/components/app-admin/app-admin-register/app-register.component.ts
+++ src/app/components/app-admin/app-admin-register/app-register.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: 'app-register.component.html',
-  styleUrls: ['app-register.component.scss']
+    selector: 'app-register',
+    templateUrl: 'app-register.component.html',
+    styleUrls: [ 'app-register.component.scss' ]
 })
-export class RegisterComponent { }
+export class RegisterComponent {
+    signUpForm: FormGroup;
+
+    constructor() {
+        this.signUpForm = new FormGroup({
+            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'password': new FormControl(null, Validators.required)
+        });
+    }
+
+    onSubmit () {
+        /*this.authenticationService.register(this.signUpForm.value)
+            .subscribe((response) => {
+                    console.log('response: ', response);
+                },
+                (data) => {
+                    this.errorService.handleError({title: data.error.error, message: data.error.error});
+                }
+            )*/
+        console.log('test');
+    };
+
+}
