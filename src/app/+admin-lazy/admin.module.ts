57d080297982af2d9f212673b63bc85c12b3ff8a
Volodymyr <wladimir.sidorow@gmail.com>
2017-12-27 18:05:43 +0200

    +

--- src/app/+admin-lazy/admin.module.ts
+++ src/app/+admin-lazy/admin.module.ts
@@ -1,18 +1,18 @@
-import {InjectionToken, NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {AdminComponent} from '../components/app-admin/app-admin.component';
-import {AdminNavComponent} from '../components/app-admin/app-admin-nav-component';
-import {LoginComponent} from '../components/app-admin/app-admin-login/app-login.component';
-import {RegisterComponent} from '../components/app-admin/app-admin-register/app-register.component';
-import {AuthenticationService} from '../data-services-admin/auth/authentication.service';
-import {AdminInteractionData} from '../data-services-admin/admin-interaction-data';
-import {AdminCabinetComponent} from '../components/app-admin/admin-content/app-admin-cabinet/app-admin-cabinet.component';
-import {AdminShowroomComponent} from '../components/app-admin/admin-content/app-admin-showroom/app-admin-showroom.component';
-import {AdminEventsComponent} from '../components/app-admin/admin-content/app-admin-events/app-admin-events.component';
-import {TranslateModule} from '@ngx-translate/core';
-import {AdminRoutingModule} from './admin-routing.module';
-import {MaterialModuleAdmin} from '../common/material.module.admin';
-import {COMPOSITION_BUFFER_MODE, FormsModule, NgForm, ReactiveFormsModule} from '@angular/forms';
+import { InjectionToken, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponent } from '../components/app-admin/app-admin.component';
+import { AdminNavComponent } from '../components/app-admin/app-admin-nav-component';
+import { LoginComponent } from '../components/app-admin/app-admin-login/app-login.component';
+import { RegisterComponent } from '../components/app-admin/app-admin-register/app-register.component';
+import { AuthenticationService } from '../data-services-admin/auth/authentication.service';
+import { AdminInteractionData } from '../data-services-admin/admin-interaction-data';
+import { AdminCabinetComponent } from '../components/app-admin/admin-content/app-admin-cabinet/app-admin-cabinet.component';
+import { AdminShowroomComponent } from '../components/app-admin/admin-content/app-admin-showroom/app-admin-showroom.component';
+import { AdminEventsComponent } from '../components/app-admin/admin-content/app-admin-events/app-admin-events.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { AdminRoutingModule } from './admin-routing.module';
+import { MaterialModuleAdmin } from '../common/material.module.admin';
+import { COMPOSITION_BUFFER_MODE, FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import {
     MAT_LABEL_GLOBAL_OPTIONS, MAT_OPTION_PARENT_COMPONENT, MAT_RIPPLE_GLOBAL_OPTIONS, MatOptgroup,
     MatOptionParentComponent, RippleGlobalOptions
