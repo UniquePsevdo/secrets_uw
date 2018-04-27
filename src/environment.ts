67913bb3594b1d0f022a776d6d68caaa19d46c2d
Volodymyr <wladimir.sidorow@gmail.com>
2018-04-27 14:51:23 +0300

    +

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+// todo: https://docs.google.com/spreadsheets/d/1Bbt5eliIFNm0PG-FV7L8LowiChaxPcpGYzZhuTGUg-8/edit#gid=337244462
 @Injectable()
 export class GlobalsDev {
     public environment: any;
